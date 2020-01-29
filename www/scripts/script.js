2de60db95161114b76dcedabb56a43c1e84500ee
kam-iwa <59604410+kam-iwa@users.noreply.github.com>
2020-01-29 18:16:58 +0100

    29.01.2020 - dodanie 4 stacji.

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,32 +1,59 @@
-require (["esri/Map", "esri/WebMap","esri/views/MapView", "esri/geometry/support/webMercatorUtils"],
-   function(Map, WebMap, MapView, webMercatorUtils)
+require (["esri/Map", "esri/WebMap","esri/views/MapView", "esri/widgets/BasemapToggle","esri/widgets/BasemapGallery", "esri/layers/GraphicsLayer", "esri/widgets/Sketch", "esri/layers/FeatureLayer"],
+   function(Map, WebMap, MapView, BasemapToggle, BasemapGallery, GraphicsLayer, Sketch, FeatureLayer)
    {
       //tutaj wywołanie funkcji//
-    let map1 = new Map({basemap:"topo"}); //słowo klucz new + nazwa modułu
-    let mapContainer = new MapView({
-        container: "mapview",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
-        map: map1,  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
-        zoom: 7,
-        center: [19,52]
-    });
+    //let map1 = new Map({basemap:"topo"}); //słowo klucz new + nazwa modułu
+    var graphicsLayer = new GraphicsLayer();
     let map2 = new WebMap({
         portalItem:{
             id:'ca06d14d225645d3b5c566c819631a3a'
-        }
+        },
+        layers: [graphicsLayer]
     })
-    let topo = document.getElementById('topo');
+    let mapContainer = new MapView({
+        container: "mapview",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
+        map: map2,  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
+        zoom: 7,
+        center: [19,52]
+    });
+    let basemapToggle = new BasemapToggle({
+        view: mapContainer,
+        nextBasemap: "satellite"
+      });
+    mapContainer.ui.add(basemapToggle, "bottom-right");
+    let basemapGallery = new BasemapGallery({
+        view: mapContainer,
+        source: {
+          portal: {
+            url: "https://www.arcgis.com",
+            useVectorBasemaps: true  // Load vector tile basemaps
+          }
+        }
+      });
+    mapContainer.ui.add(basemapGallery, "bottom-left");
+    let sketch = new Sketch({
+        view: mapContainer,
+        layer: graphicsLayer
+    });
+    mapContainer.ui.add(sketch, "top-right");
+    let trailheadsLayer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/USA%20States/FeatureServer/0"
+    });
+
+    map2.add(trailheadsLayer);
+
+    /*let topo = document.getElementById('topo');
     let satellite = document.getElementById('satellite');
     let osm = document.getElementById('osm');
-    let agol = document.getElementById('agol');
-    let photo = document.getElementsByTagName('img');
-    //console.log(photo);
+    let agol = document.getElementById('agol');*/
+    let photo = document.getElementsByClassName('image');
     
 
     /*mapContainer.on("click", function(evt){
         let coords = webMercatorUtils.webMercatorToGeographic(evt.mapPoint);
         //console.log('x : '+ coords.x.toFixed(5) +', '+ coords.y.toFixed(5));
     });*/
-    topo.addEventListener('click', function(){
+    /* topo.addEventListener('click', function(){
         if(mapContainer.map !== map1){
             mapContainer = new MapView({
                 container: "mapview",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
@@ -60,8 +87,8 @@ require (["esri/Map", "esri/WebMap","esri/views/MapView", "esri/geometry/support
         map1.basemap = 'osm';
     })
     agol.addEventListener('click', function(){
-        mapContainer.map = map2;
-    })
+        mapContainer.map = map2; 
+    })*/
     for(let i = 0; i < photo.length; i++){
         photo[i].addEventListener('click', function(){
             if (photo[i].alt === 'Żulin'){
@@ -96,8 +123,24 @@ require (["esri/Map", "esri/WebMap","esri/views/MapView", "esri/geometry/support
                 mapContainer.zoom = 16;
                 mapContainer.center = [19.46941, 51.77002];
             }
+            else if (photo[i].alt === 'Chojnice'){
+                mapContainer.zoom = 16;
+                mapContainer.center = [17.575627777778, 53.687822222222];
+            }
+            else if (photo[i].alt === 'Łomża'){
+                mapContainer.zoom = 16;
+                mapContainer.center = [22.065805555556, 53.173472222222];
+            }
+            else if (photo[i].alt === 'Zakopane'){
+                mapContainer.zoom = 16;
+                mapContainer.center = [19.962777777778, 49.301111111111];
+            }
+            else if (photo[i].alt === 'Inowrocław Rąbinek'){
+                mapContainer.zoom = 16;
+                mapContainer.center = [18.242658333333, 52.769541666667];
+            }
 
-        })
+        });
     }
 
 
